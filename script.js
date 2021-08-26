222b7c4232f27de1358882ec6db747c3b3f6bb43
alt-art <pedromendescraft@gmail.com>
2021-08-26 15:42:41 -0300

    Fix don't show forks

--- script.js
+++ script.js
@@ -14,25 +14,27 @@ fetch("https://api.github.com/users/alt-art/repos?type=owner&sort=created").then
     return res.json();
 }).then(data => {
     data.forEach(repo => {
-        let link = document.createElement("a");
-        let article = document.createElement("article");
-        let title = document.createElement("h3");
-        let description = document.createElement("p");
-        title.innerText = repo.name;
-        description.innerText = repo.description;
-        link.href = repo.html_url;
-        article.append(title);
-        article.append(description);
-        if (repo.language != null) {
-            let tag = document.createElement("span");
-            tag.className = "tag";
-            tag.innerText = repo.language;
-            article.append(tag);
+        if (!repo.fork){
+            let link = document.createElement("a");
+            let article = document.createElement("article");
+            let title = document.createElement("h3");
+            let description = document.createElement("p");
+            title.innerText = repo.name;
+            description.innerText = repo.description;
+            link.href = repo.html_url;
+            article.append(title);
+            article.append(description);
+            if (repo.language != null) {
+                let tag = document.createElement("span");
+                tag.className = "tag";
+                tag.innerText = repo.language;
+                article.append(tag);
+            }
+            link.append(article);
+            link.target = "_blank";
+            link.rel = "noreferrer";
+            articles.append(link);
         }
-        link.append(article);
-        link.target = "_blank";
-        link.rel = "noreferrer";
-        articles.append(link);
     })
 }).catch(reason => {
     console.error("Error when requesting repos from alt-art", reason);
