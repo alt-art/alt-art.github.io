57aed639b5150a00427905ac4ea93dbe34f6c282
alt-art <pedromendescraft@gmail.com>
2021-10-18 18:57:21 -0300

    Refactoring: one line callback, image create after load and separating in addRepo function

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
-let articles = document.querySelector(".articles");
-let avatar_img = document.querySelector(".avatar");
+const articles = document.querySelector('.articles');
+const avatarDiv = document.querySelector('.avatar');
+fetch('https://api.github.com/users/alt-art')
+  .then((res) => res.json())
+  .then((user) => {
+    const img = document.createElement('img');
+    img.src = user.avatar_url;
+    img.alt = 'Github avatar';
+    avatarDiv.appendChild(img);
+  })
+  .catch((reason) => {
+    console.error('Error when requesting avatar image from alt-art', reason);
+  });
 
-fetch("https://api.github.com/users/alt-art").then(res => {
-    return res.json();
-}).then(user => {
-    avatar_img.src = user.avatar_url;
-}).catch(reason => {
-    console.error("Error when requesting avatar image from alt-art", reason);
-});
+fetch('https://api.github.com/users/alt-art/repos?type=owner&sort=updated')
+  .then((res) => res.json())
+  .then((data) => {
+    data
+      .filter((repo) => !repo.fork)
+      .forEach((repo) => {
+        addRepo(repo);
+      });
+  })
+  .catch((reason) => {
+    console.error('Error when requesting repos from alt-art\n', reason);
+  });
 
-
-fetch("https://api.github.com/users/alt-art/repos?type=owner&sort=updated").then(res => {
-    return res.json();
-}).then(data => {
-    data.forEach(repo => {
-        if (!repo.fork) {
-            let link = document.createElement("a");
-            let article = document.createElement("article");
-            let title = document.createElement("h3");
-            let description = document.createElement("p");
-            title.innerText = repo.name;
-            description.innerText = repo.description;
-            link.href = repo.html_url;
-            article.append(title);
-            article.append(description);
-            if (repo.language != null) {
-                let tag = document.createElement("span");
-                tag.className = "tag";
-                tag.innerText = repo.language;
-                article.append(tag);
-            }
-            link.append(article);
-            link.target = "_blank";
-            link.rel = "noreferrer";
-            articles.append(link);
-        }
-    })
-}).catch(reason => {
-    console.error("Error when requesting repos from alt-art", reason);
-});
+function addRepo(repo) {
+  const link = document.createElement('a');
+  const article = document.createElement('article');
+  const title = document.createElement('h3');
+  const description = document.createElement('p');
+  title.innerText = repo.name;
+  description.innerText = repo.description;
+  link.href = repo.html_url;
+  article.append(title);
+  article.append(description);
+  if (repo.language != null) {
+    const tag = document.createElement('span');
+    tag.className = 'tag';
+    tag.innerText = repo.language;
+    article.append(tag);
+  }
+  link.append(article);
+  link.target = '_blank';
+  link.rel = 'noreferrer';
+  articles.append(link);
+}
