2adc35907453f4ffc62786c46cfed9cc15003036
Pedro Mendes <pedromendescraft@gmail.com>
2023-03-09 20:07:22 -0300

    fix: Add env variable for development

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,17 +1,24 @@
 import axios from 'axios';
 
 interface Repository {
-  name: string
-  description: string
-  html_url: string
-  thumb: string
+  name: string;
+  description: string;
+  html_url: string;
+  thumb: string;
 }
 
 export async function getRepositories(): Promise<Repository[]> {
   if (!import.meta.env.VITE_REPO_API) {
     throw new Error('Repository data URL is not defined');
   }
-  const repositories = (await axios.get<Repository[]>(import.meta.env.VITE_REPO_API))
-    .data;
+  const repositories = (
+    await axios.get<Repository[]>(import.meta.env.VITE_REPO_API, {
+      headers: {
+        Authorization: import.meta.env.VITE_DEV_TOKEN
+          ? `Bearer ${import.meta.env.VITE_DEV_TOKEN as string}`
+          : false,
+      },
+    })
+  ).data;
   return repositories;
 }
